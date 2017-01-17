626f53846139555fce69b7390a4430358b474665
Amuthan Arulraj <arulraj.amuthan@gmail.com>
2017-01-17 17:14:45 -0500

    Adding testng test cases

--- src/test/java/testng/BinarySearchTest.java
+++ src/test/java/testng/BinarySearchTest.java
@@ -1,16 +1,18 @@
 package testng;
 
+import static org.testng.Assert.*;
+
+import java.util.Arrays;
+
 import org.testng.annotations.Test;
-import org.testng.annotations.BeforeMethod;
 
 public class BinarySearchTest {
-  
-  @BeforeMethod
-  public void beforeMethod() {
-  }
 	
-  @Test
-  public void f() {
+  @Test(dataProvider="sortedArrays", dataProviderClass=BinarySearchDataProvider.class)
+  public void findValue(Integer[] sortedArray) {
+	  for (int index=0; index < sortedArray.length; index++) {
+		  assertEquals(Arrays.binarySearch(sortedArray, sortedArray[index]), index);
+	  }
   }
 
 }
