4f9489db81f07cbc3e3d8537bfce1fba5016257f
Amuthan Arulraj <arulraj.amuthan@gmail.com>
2017-01-17 10:34:40 -0500

    Adding base testng project

---
+++ src/test/java/testng/BinarySearchTest.java
@@ -0,0 +1,16 @@
+package testng;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeMethod;
+
+public class BinarySearchTest {
+  
+  @BeforeMethod
+  public void beforeMethod() {
+  }
+	
+  @Test
+  public void f() {
+  }
+
+}
